3d2ef20e2f40d5dc05025e271f61fffc2a4a782d
Pukich-Kakich <159930290+Pukich-Kakich@users.noreply.github.com>
2024-02-14 02:23:01 +1000

    one commit

---
+++ Script.ts
@@ -0,0 +1,31 @@
+// let number: number = 13;
+// const constanta: number = 3.14;
+// let string: string = "negr";
+// let arr: string [] = ["jhhk", "YUuyuuttu"];
+// let func: ()=>void=()=>{};
+// ------------------------------------------------
+
+// class Point {x:number; y:number;}
+// let point: Point = new Point ()
+// point.x = 1;
+// point.y = 3;
+// class Rect {
+//     private x1: number 
+//     public x2: number 
+//     y1: number
+//     y2: number
+//     private readonly MAX_COORD = 1000;
+//     constructor(x: number, y: number) {
+//         this.x1 = x
+//         this.x2 = x
+//         this.y1 = y
+//         this.y2 = y
+//     }
+//     square(){
+//         return Math.abs(this.x1 - this.x2) * Math.abs(this.y1 - this.y2)
+//     }
+// }
+// let rect1: Rect = new Rect(10, 20);
+// let restSquare: number = rect1.square();
+// console.log(restSquare)
+// -------------------------------------------------------------------------------
